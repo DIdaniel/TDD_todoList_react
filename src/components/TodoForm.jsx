bdde29eccdf852a92a45f333f7e7cedb1c47ec8e
KIM DONG IL <cappu159@gmail.com>
2021-12-06 11:19:01 +0900

    TodoForm test

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ onInsert }) => {
+  const [value, setValue] = useState("");
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue("");
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form>
-      <input type="text" placeholder="할 일을 입력하세요" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">등록</button>
     </form>
   );
--- src/components/TodoForm.test.js
+++ src/components/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm />", () => {
@@ -7,5 +7,37 @@ describe("<TodoForm />", () => {
     const { getByText, getByPlaceholderText } = render(<TodoForm />);
     getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
     getByText("등록"); // button이 있는지 확인
+  });
+
+  it("changes input", () => {
+    const { getByPlaceholderText } = render(<TodoForm />);
+    const input = getByPlaceholderText("할 일을 입력하세요");
+    // eslint-disable-next-line no-undef
+    fireEvent.change(input, {
+      target: {
+        value: "TDD with vlpt",
+      },
+    });
+    expect(input).toHaveAttribute("value", "TDD with vlpt");
+  });
+
+  it("calls onInsert and clears input", () => {
+    const onInsert = jest.fn();
+    const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
+
+    const input = getByPlaceholderText("할 일을 입력하세요");
+    const button = getByText("등록");
+
+    // 수정하기
+    fireEvent.change(input, {
+      target: {
+        value: "TDD with vlpt",
+      },
+    });
+
+    // 버튼 클릭
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("TDD with vlpt"); // onInsert가 "TDD with vlpt" 파라미터가 호출됐어야함
+    expect(input).toHaveAttribute("value", ""); // input이 비워져야함
   });
 });
