58aedb8c092227acefd2228c96a359ebcd335e0f
KIM DONG IL <cappu159@gmail.com>
2021-12-06 15:53:25 +0900

    TodoItem_Test

--- src/index.js
+++ src/index.js
@@ -11,9 +11,11 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// https://learn-react-test.vlpt.us/#/06-rtl-tdd?id=%ec%83%88-%ed%94%84%eb%a1%9c%ec%a0%9d%ed%8a%b8-%eb%a7%8c%eb%93%a4%ea%b8%b0
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// https://learn-react-test.vlpt.us/#/06-rtl-tdd?id=%ec%83%88-%ed%94%84%eb%a1%9c%ec%a0%9d%ed%8a%b8-%eb%a7%8c%eb%93%a4%ea%b8%b0
+
+// TodoItem 만들기
