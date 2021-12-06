055863913de493885c9a32cf0653e7cbab8b5049
KIM DONG IL <cappu159@gmail.com>
2021-12-06 11:28:18 +0900

    TodoForm test refactoring

--- src/components/TodoForm.test.js
+++ src/components/TodoForm.test.js
@@ -3,30 +3,41 @@ import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm />", () => {
+  const setup = (props = {}) => {
+    const utils = render(<TodoForm {...props} />);
+    const { getByText, getByPlaceholderText } = utils;
+    const input = getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
+    const button = getByText("등록"); // button이 있는지 확인
+    return {
+      ...utils,
+      input,
+      button,
+    };
+  };
+
   it("has input and a button", () => {
-    const { getByText, getByPlaceholderText } = render(<TodoForm />);
-    getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
-    getByText("등록"); // button이 있는지 확인
+    const { input, button } = setup();
+    expect(input).toBeTruthy(); // 해당 값이 Truthy 한 값인지 확인
+    expect(button).toBeTruthy();
+    // const { getByText, getByPlaceholderText } = render(<TodoForm />);
+    // getByPlaceholderText("할 일을 입력하세요"); // input이 있는지 확인
+    // getByText("등록"); // button이 있는지 확인
   });
 
   it("changes input", () => {
-    const { getByPlaceholderText } = render(<TodoForm />);
-    const input = getByPlaceholderText("할 일을 입력하세요");
-    // eslint-disable-next-line no-undef
+    const { input } = setup();
     fireEvent.change(input, {
       target: {
         value: "TDD with vlpt",
       },
     });
+
     expect(input).toHaveAttribute("value", "TDD with vlpt");
   });
 
   it("calls onInsert and clears input", () => {
     const onInsert = jest.fn();
-    const { getByText, getByPlaceholderText } = render(<TodoForm onInsert={onInsert} />);
-
-    const input = getByPlaceholderText("할 일을 입력하세요");
-    const button = getByText("등록");
+    const { input, button } = setup({ onInsert });
 
     // 수정하기
     fireEvent.change(input, {
